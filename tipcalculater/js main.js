e48b7b543321dc9fca64297fe26d1fe1b6096e6a
solomonkaregay <88211239+solomonkaregay@users.noreply.github.com>
2021-10-30 02:43:07 -0500

    fanal

---
+++ tipcalculater/js main.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('calcTip',function(){
+let subtotal = document.getElementById('subtotal').Value;
+let tippercentage = document.getElementById('tip').Value; 
+document.getElementById('Calculate Tip').Value=parseFloat(subtotal)+parseFloat(tip);
+})
+
+//document.getElementById("calcula").onclick = function() {
+   // calculateTip();
+
+ 
